e2bf6fdcc060bd914050f84186176fff3bd59420
tevesgithub <vjteves@gmail.com>
2024-07-31 13:03:32 -0600

    comment

--- App.js
+++ App.js
@@ -1,1 +1,12 @@
-console.log('Hello,World!');
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req,res) => {
+    res.send('Hello, world!');
+
+});
+app.listen(3000, () =>{
+    console.log('Server  started on port ${PORT}');
+});
