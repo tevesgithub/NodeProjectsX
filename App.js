a75683098144291dc920f98156cdb6408f4d8647
tevesgithub <vjteves@gmail.com>
2024-08-01 10:48:10 -0600

    first commit

--- App.js
+++ App.js
@@ -3,6 +3,17 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+/*middleware
+app.use((req,res, next) => {
+    console.log('Middleware Executed!');
+    next();
+});*/
+
+app.get('/users/:id', (req,res) => {
+    const userId = req.params.id;
+    res.send (`User with ID ${userId}`);
+});
+
 app.get('/', (req,res) => {
     res.send('Hello, world!');
 
